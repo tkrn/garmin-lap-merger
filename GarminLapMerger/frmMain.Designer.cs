724210c7723991d9fd7bb21bc73cfd2a3ac82059
Patrick Stasko <patrick.stasko@outlook.com>
2014-10-16 18:18:21 -0400

    Init of new structure with proper .gitignore

---
+++ GarminLapMerger/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GarminLapMerger
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.lstSelectedFiles = new System.Windows.Forms.ListBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.lnkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Location = new System.Drawing.Point(12, 14);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(117, 23);
+            this.btnBrowseFolder.TabIndex = 0;
+            this.btnBrowseFolder.Text = "Browse Folder";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // lstSelectedFiles
+            // 
+            this.lstSelectedFiles.FormattingEnabled = true;
+            this.lstSelectedFiles.Location = new System.Drawing.Point(12, 43);
+            this.lstSelectedFiles.MultiColumn = true;
+            this.lstSelectedFiles.Name = "lstSelectedFiles";
+            this.lstSelectedFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstSelectedFiles.Size = new System.Drawing.Size(445, 95);
+            this.lstSelectedFiles.TabIndex = 1;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(135, 16);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(322, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "BrowseFolderPath";
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMerge.Location = new System.Drawing.Point(333, 144);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(126, 23);
+            this.btnMerge.TabIndex = 0;
+            this.btnMerge.Text = "Merge as New File";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "tcx";
+            this.saveFileDialog.FileName = "Combined.tcx";
+            this.saveFileDialog.Filter = "Garmin TCX File|*.tcx";
+            this.saveFileDialog.SupportMultiDottedExtensions = true;
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.Description = "Select the folder which contains the Garmin TCX GPS files.";
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // lnkLabel
+            // 
+            this.lnkLabel.AutoSize = true;
+            this.lnkLabel.Location = new System.Drawing.Point(12, 149);
+            this.lnkLabel.Name = "lnkLabel";
+            this.lnkLabel.Size = new System.Drawing.Size(115, 13);
+            this.lnkLabel.TabIndex = 2;
+            this.lnkLabel.TabStop = true;
+            this.lnkLabel.Text = "www.nerdthinking.com";
+            this.lnkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLabel_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 177);
+            this.Controls.Add(this.lnkLabel);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.lstSelectedFiles);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Garmin Lap Merger - Version 1.0";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.ListBox lstSelectedFiles;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.LinkLabel lnkLabel;
+    }
+}
+
---
+++ GarminLapMerger/frmMain.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GarminLapMerger
+{
+    public partial class frmMain : Form
+    {
+        private String Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtPath.Text = Path;
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            ProcessXMLFiles();
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                Path = folderBrowserDialog.SelectedPath.ToString();
+                txtPath.Text = Path;
+                PopulateFileList();
+            }  
+        }
+
+        private void PopulateFileList()
+        {
+            lstSelectedFiles.Items.Clear();
+
+            try
+            {         
+                string[] Files = Directory.GetFiles(Path, "*.tcx");
+
+                foreach (string File in Files)
+                {
+                    string[] FileName = File.Split('\\');
+
+                    lstSelectedFiles.Items.Add(FileName[FileName.Length-1]);        
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+
+        private void ProcessXMLFiles()
+        {
+            if (lstSelectedFiles.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("No files selected!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (lstSelectedFiles.SelectedItems.Count == 1)
+            {
+                MessageBox.Show("Select more than one file to merge!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else //ioif (lstSelectedFiles.SelectedItems.Count > 1) 
+            {
+                ListBox.SelectedObjectCollection SelectedItems = lstSelectedFiles.SelectedItems;
+
+                //We are going to use a List<T> instead of ArrayList
+                //also we are going to use Tuple<DateTime, String> for the items
+                var LapsList = new List<Tuple<DateTime, String, String>>();
+
+                foreach (String Selected in SelectedItems)
+                {
+                    String FullPath = Path + @"\" + Selected;
+                    XmlDocument xDoc = new XmlDocument();
+
+                    try
+                    {
+                        xDoc.Load(FullPath);
+
+                        XmlNodeList Laps = xDoc.GetElementsByTagName("Lap");
+
+                        foreach (XmlElement Lap in Laps)
+                        {
+                            DateTime StartTime = DateTime.Parse(Lap.Attributes[0].Value);
+                            String XML = Lap.InnerXml.ToString();
+
+                            //Here we create the tuple and add it
+                            LapsList.Add(new Tuple<DateTime, String, String>(StartTime, XML, FullPath));
+                        }
+
+                    }
+                    catch (IOException e)
+                    {
+                        MessageBox.Show("Error loading file: " + FullPath + "\r\n" + "Exception: " + e, "Error loading file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                //We are sorting with Linq
+                LapsList = LapsList.OrderBy(lap => lap.Item1).ToList();
+
+                StreamReader MetaDataXML = new StreamReader(LapsList[0].Item3);
+                String XMLContentsString = MetaDataXML.ReadToEnd();
+
+                int HeaderEndPosition = XMLContentsString.IndexOf("</Id>") + 5;
+                int FooterStartPosition = XMLContentsString.IndexOf(@"<Creator xsi:type=""Device_t"">");
+
+                String Header = XMLContentsString.Substring(0, HeaderEndPosition);
+                String Footer = XMLContentsString.Substring(FooterStartPosition);
+
+                XmlDocument NewXMLDocument = new XmlDocument();
+
+                String NewFile = Header;
+
+                foreach (Tuple<DateTime, String, String> Lap in LapsList)
+                {
+
+                    string CurrentLap = "<Lap StartTime=\"" + Lap.Item1.ToString("yyyy-MM-ddTHH:mm:ssZ") + "\">";
+                    CurrentLap += Lap.Item2;
+                    CurrentLap += "</Lap>";
+
+                    NewFile += CurrentLap;
+
+                }
+
+                NewFile += Footer;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        byte[] ByteData = Encoding.ASCII.GetBytes(NewFile);
+                        FileStream SaveFileStream = (FileStream)saveFileDialog.OpenFile();
+                        SaveFileStream.Write(ByteData, 0, ByteData.Length);
+                        SaveFileStream.Close();
+
+                        MessageBox.Show("File written successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("File failed to write!", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void lnkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.nerdthinking.com");
+        }
+
+    }
+}
